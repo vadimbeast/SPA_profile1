d573a9c63d597c29f4589b62122b7aef2caa4de7
vadimbeast <vadim.reklama58@yandex.ru>
2023-04-13 07:46:13 +0300

    added map method for Dialogs and MyPost pages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,20 +20,41 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogs = [
+        {id: 1, name: 'Masha'},
+        {id: 2, name: 'Max'},
+        {id: 3, name: 'Mama'},
+        {id: 4, name: 'Papa'},
+        {id: 5, name: 'Sasha'},
+        {id: 6, name: 'Misha'}
+    ]
+
+    let messages = [
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How r u?'},
+        {id: 3, message: 'I am fine'},
+        {id: 4, message: 'Go to  Shishky'}
+    ]
+
+
+    let dialogsElements = dialogs
+        .map( d => <DialogItem name = {d.name} id = {d.id}/> );
+
+    let messagesElements = messages
+        .map( m => <Message message = {m.message}/> )
+
     return (
         <div className={s.dialogs}>
            <div className={s.dialogsItems}>
 
-               <DialogItem name="Masha" id="1"/>
-               <DialogItem name="Max" id="2"/>
-               <DialogItem name="Mama" id="3"/>
-               <DialogItem name="Papa" id="4"/>
-               <DialogItem name="Sasha" id="5"/>
-               <DialogItem name="Misha" id="6"/>
+               { dialogsElements }
 
            </div>
-           <div className={s.dialogs}>
-               <Message message="Hi"/> 
+           <div className={s.dialogsItems}>
+               
+               { messagesElements }
+
            </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,15 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+  let postData = [
+    {id: 1, message: 'Hi, how are you?', likesCount: 12},
+    {id: 2, message: 'It is me first post', likesCount: 7},   
+  ]
+
+  let postElements = 
+      postData.map( p => <Post message={p.message} likesCount={p.likesCount}/> )
+
     return (
         <div className={s.postBlock}>
         <h3>My post</h3>
@@ -10,19 +19,12 @@ const MyPosts = () => {
                <textarea></textarea>
                <div>
                    <button>Add post</button>
-               </div>
-               
-               <button>Remove</button>
-               
+               </div>              
+               <button>Remove</button>    
            </div>
-        <div>
-          new post 
-        </div>
-        <div>
-          <Post message='How are you?' />
-          <div className={s.postBlock}>
-            post 2
-          </div>
+        
+        <div className={s.postBlock}>
+          { postElements }
         </div>
       </div>
     );
